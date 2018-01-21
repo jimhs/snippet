ba7e026e815cb0ce4aba284f872473a8092b0b43
jimhs <pandabeer@vip.qq.com>
2018-01-21 00:55:08 -0500

    Update collected.sh

--- wheels/bash/collected.sh
+++ wheels/bash/collected.sh
@@ -2,3 +2,7 @@
 
 # history汇总
 cat .bash_history | awk '{print $1}' | sort | uniq -c | sort -n > ~/bash/.bash_hitory_trimmed
+
+# link crawler
+# how to multi-threads?
+lynx -dump http://www.mutt.org/screenshots/ | awk '/http/{print $2}' | grep gif | wget -i gifs
