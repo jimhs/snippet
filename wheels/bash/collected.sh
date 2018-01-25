dd7e3ad93af0b598b9efa088993c7e6b9be39944
jimhs <pandabeer@vip.qq.com>
2018-01-25 03:37:36 -0500

    Update collected.sh

--- wheels/bash/collected.sh
+++ wheels/bash/collected.sh
@@ -14,3 +14,37 @@ lynx -dump http://www.mutt.org/screenshots/ | awk '/http/{print $2}' | grep gif 
 
 # 搜狗拼音输入法异常错误时，重启fcitx
 ps aux | grep fcitx | awk 'NR==1 {print $2}' | xargs kill
+
+# wifi problem
+/etc/modprobe.d/rtl8723be.conf
+
+[ver1](http://www.linuxidc.com/Linux/2016-05/131150.htm)
+
+```
+options rtl8723be debug=1
+options rtl8723be disable_watchdog=N
+options rtl8723be fwlps=Y
+options rtl8723be ips=Y
+options rtl8723be msi=N
+options rtl8723be swenc=N
+options rtl8723be swlps=N
+options rtl8723be ant_sel=2
+```
+
+[or?](http://blog.csdn.net/chenhao0428/article/details/51885805)
+
+```
+options rtl8723be ant_sel=2
+
+options rtl8723be ips=0
+options rtl8723be fwlps=0
+options rtl8723be swenc=1
+```
+
+```
+lspci | grep Wireless
+
+sudo modinfo rtl8723be
+
+modprobe -r rtl8723be
+modprobe rtl8723be
