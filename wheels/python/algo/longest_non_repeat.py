bf764222f7efe5f0a6c341f7d4e86e8ab211863e
jimhs <foun_da@sina.cn>
2018-05-02 12:19:40 -0400

    @May.02

---
+++ wheels/python/algo/longest_non_repeat.py
@@ -0,0 +1,26 @@
+"""
+longest substr w/o repeating chars
+
+todo@may.02: why 'for' loop not working?
+"""
+
+
+def lnr(s):
+    """
+    in:
+        s: a string
+    ou:
+        ss: lnr
+    """
+    i = 0
+    ss = []
+    u = []
+    while i < len(s):
+        if s[i] not in u:
+            u.append(s[i])
+            i = i + 1
+            continue
+        if len(u) > len(ss):
+            ss = u[:]
+            u = []
+    return ss
