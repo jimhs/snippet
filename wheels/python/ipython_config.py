9ebd078922138c0815611d6ba6f37471e3652cda
jimhs <foun_da@sina.cn>
2018-05-06 06:19:16 -0400

    @May.06

---
+++ wheels/python/ipython_config.py
@@ -0,0 +1,17 @@
+"""
+from Wes
+"""
+
+import sys
+
+
+def set_trace():
+    from IPython.core.debugger import Pdb
+    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+
+
+def debug(f, *args, **kargs):
+    from IPython.core.debugger import Pdb
+    pdb = Pdb(color_scheme='Linux')
+    return pdb.runcall(f, *args, **kargs)
+
